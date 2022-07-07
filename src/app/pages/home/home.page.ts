a19f5f2efb857eba894890d9e54748fff85107d1
Willy Jhoan Pinto <73348914+willpinto@users.noreply.github.com>
2022-07-07 09:48:20 -0500

    ion list prueba

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Componente{
+  icon: string;
+  name: string;
+  redirecTo: string;
+}
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+
+  componentes: Componente[] = [
+    {
+      icon: 'tv-outline',
+      name: 'about',
+      redirecTo: '/about'
+    },
+    {
+      icon: 'alert-outline',
+      name: 'alert',
+      redirecTo: '/alert'
+    }
+  ];
 
   constructor() { }
 
