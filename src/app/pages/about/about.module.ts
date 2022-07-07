d51962fcec6a5c6389e17f1b46974f914e5a1bea
Willy Jhoan Pinto <73348914+willpinto@users.noreply.github.com>
2022-07-07 08:21:28 -0500

    componentes

--- src/app/pages/about/about.module.ts
+++ src/app/pages/about/about.module.ts
@@ -1,3 +1,4 @@
+import { ComponentsModule } from './../../components/components.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -13,7 +14,8 @@ import { AboutPage } from './about.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    AboutPageRoutingModule
+    AboutPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [AboutPage]
 })
